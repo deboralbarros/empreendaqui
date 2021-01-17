5887dda81e0a56dcf18db278158b2784a8bb7ae2
Débora Barros <debora_lbarros@outlook.com>
2021-01-17 16:35:44 -0400

    feat: logo and description

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
-import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
