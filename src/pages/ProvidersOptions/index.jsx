64731b68da1cf95281deca50d258fc1c10c2d600
stefanycoimbra <56750569+stefanycoimbra@users.noreply.github.com>
2021-01-17 14:51:06 -0300

    ./src/pages/Profile/index.jsx

---
+++ src/pages/ProvidersOptions/index.jsx
@@ -0,0 +1,62 @@
+import { useHistory } from "react-router-dom";
+
+import { Container, Form, Label, Select, Title, Applogo, Space } from "./style";
+
+import Button from "../../components/Button";
+import Footer from "../../components/Footer";
+
+import logoFornecedores from '../../images/GroupFornecedores.png';
+
+const HomeProvider = () => {
+    const history = useHistory();
+
+    const navigateToSearchProviders = (e) => {
+        e.preventDefault();
+
+        history.push("/searchproviders");
+    };
+
+    const navigateToCreateBid = (e) => {
+        e.preventDefault();
+
+        history.push("/createbid");
+    };
+
+    return (
+        <Container>
+            <Form>
+
+                <Title>
+                    Fornecedores
+                </Title>
+
+                
+                <Button
+                    background="#4FC18E"
+                    width="80%"
+                    onClick={navigateToSearchProviders}
+                >
+                    LISTA DE FORNECEDORES
+                </Button>
+
+                <Space></Space>
+
+                <Button
+                    background="#4FC18E"
+                    width="80%"
+                    onClick={navigateToCreateBid}
+                >
+                    CRIAR PROCURA DE PRODUTO
+                </Button>
+            </Form>
+                    
+            <Applogo 
+                src={logoFornecedores} alt="logoFornecedores"
+            >
+            </Applogo>
+
+        </Container>
+    );
+};
+
+export default HomeProvider;
